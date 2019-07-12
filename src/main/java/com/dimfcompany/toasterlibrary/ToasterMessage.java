af58bd22340e8f2dcc6b277d164da67bf5de83a3
bios90 <bios90@mail.ru>
2019-07-12 16:14:16 +0300

    Test release 2

--- src/main/java/com/dimfcompany/toasterlibrary/ToasterMessage.java
+++ src/main/java/com/dimfcompany/toasterlibrary/ToasterMessage.java
@@ -7,6 +7,6 @@ public class ToasterMessage
 {
     public static void s(Context c, String message)
     {
-        Toast.makeText(c, message, Toast.LENGTH_SHORT).show();
+        Toast.makeText(c, message+"Tra la la edited***", Toast.LENGTH_SHORT).show();
     }
 }
